6ba6147480109407ab5edc822e34500c9f4068f3
Louise Duff <louiseduff@MacBook-Air-3.local>
2023-08-23 19:46:33 +0100

    my first handler and struct

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-func main () {
-	
-}
+import (
+	"net/http"
 
+	"github.com/Louduff/RECIPEKEEPER/handlers"
+)
+
+func main() {
+	http.HandleFunc("/", handlers.HomePage)
+	http.ListenAndServe(":8000", nil)
 }
